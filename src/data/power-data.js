dc1c5699328dfd78d3ef139263652e6cecb7b286
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-24 17:34:08 -0500

    add powers data

---
+++ src/data/power-data.js
@@ -0,0 +1,44 @@
+export const Powers = {
+  1: {
+    id: 1,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  },
+  2: {
+    id: 2,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  },
+  3: {
+    id: 3,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  },
+  4: {
+    id: 4,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  },
+  5: {
+    id: 5,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  },
+  6: {
+    id: 6,
+    name: "smokeScreen",
+    price: 100,
+    used: 0,
+    img: "./smokeScreen.png"
+  }
+};
