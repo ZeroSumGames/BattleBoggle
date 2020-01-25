9fadd1952d53a19ef54c17e7c56bc5e2a12bf2bf
jasenvchan <jachan@gwu.edu>
2020-01-24 22:14:02 -0500

    submitting and updating scores works but adding letter score to word score does not

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,8 @@ import {connect} from 'react-redux'
 import LetterTile from './LetterTile'
 import {linearize} from '../utilities'
 import ReactDOM from 'react-dom'
+import {submitWord, clearWordScore, addP1Score, clearWord} from '../store/game'
+
 // import {makeBoard} from '../store/game'
 
 class Board extends React.Component {
@@ -23,11 +25,22 @@ class Board extends React.Component {
 		this.setState({mouseIsDown: !this.state.mouseIsDown})
 	}
 
+	toggleMouseUp(event) {
+		this.setState({mouseIsDown: !this.state.mouseIsDown})
+		// this.props.checkWord(this.props.wordScore)
+
+		this.props.addP1Score(this.props.wordScore)
+		this.props.clearWord()
+		this.props.clearWordScore()
+
+		console.log("fdsfsdf");
+	}
+
 	render() {
 		let board = this.props.board;
 
 		return (
-			<div onMouseDown={(event) => this.toggleMouseDown(event)} onMouseUp={(event) => this.toggleMouseDown(event)}>
+			<div onMouseDown={(event) => this.toggleMouseDown(event)} onMouseUp={(event) => this.toggleMouseUp(event)}>
 				{board && board.map((letter,i) => {
 					return (
 						<LetterTile mouseIsDown={this.state.mouseIsDown} letter={letter} key={i} row={Math.floor(i/4)} col={i%4}/>
@@ -40,12 +53,18 @@ class Board extends React.Component {
 
 const mapStateToProps = state => {
 	return {
-		board: state.board
+		board: state.board,
+		wordScore: state.wordScore
 	}
 }
 
 const mapDispatchToProps = dispatch => {
-	return {}
+	return {
+		checkWord: wordScore => submitWord(wordScore),
+		addP1Score: wordScore => dispatch(addP1Score(wordScore)),
+		clearWord: () => dispatch(clearWord()),
+		clearWordScore: () => dispatch(clearWordScore())
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Board);
--- src/store/game.js
+++ src/store/game.js
@@ -4,7 +4,12 @@
 const BUILD_BOARD = 'BUILD_BOARD'
 const INITIALIZE_LETTERS = 'INITIALIZE_LETTERS'
 const ADD_LETTER = 'ADD_LETTER'
-const USE_POWER = "USE_POWER";
+const USE_POWER = 'USE_POWER'
+const CLEAR_WORD = 'CLEAR_WORD'
+const ADD_LETTER_SCORE = 'ADD_LETTER_SCORE'
+const CLEAR_WORD_SCORE = 'CLEAR_WORD_SCORE'
+const ADD_P1_SCORE = 'ADD_P1_SCORE'
+const CLEAR_P1_SCORE = 'CLEAR_P1_SCORE'
 
 
 // MAKER FUNCTIONS
@@ -29,81 +34,84 @@ export function makeBoard(letters) {
 // INITIAL STATE
 
 const initialState = {
-  letters: {
-    A: { id: 0, value: "A", points: 1 },
-    B: { id: 1, value: "B", points: 4 },
-    C: { id: 2, value: "C", points: 4 },
-    D: { id: 3, value: "D", points: 2 },
-    E: { id: 4, value: "E", points: 1 },
-    F: { id: 5, value: "F", points: 4 },
-    G: { id: 6, value: "G", points: 3 },
-    H: { id: 7, value: "H", points: 3 },
-    I: { id: 8, value: "I", points: 1 },
-    J: { id: 9, value: "J", points: 10 },
-    K: { id: 10, value: "K", points: 5 },
-    L: { id: 11, value: "L", points: 3 },
-    M: { id: 12, value: "M", points: 4 },
-    N: { id: 13, value: "N", points: 2 },
-    O: { id: 14, value: "O", points: 1 },
-    P: { id: 15, value: "P", points: 4 },
-    Q: { id: 16, value: "Q", points: 10 },
-    R: { id: 17, value: "R", points: 1 },
-    S: { id: 18, value: "S", points: 1 },
-    T: { id: 19, value: "T", points: 1 },
-    U: { id: 20, value: "U", points: 2 },
-    V: { id: 21, value: "V", points: 5 },
-    W: { id: 22, value: "W", points: 4 },
-    X: { id: 23, value: "X", points: 1 },
-    Y: { id: 24, value: "Y", points: 1 },
-    Z: { id: 25, value: "Z", points: 10 }
-  },
-  board: {},
-  powers: [
-    {
-      id: 1,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png"
-    },
-    {
-      id: 2,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png" // switch
-    },
-    {
-      id: 3,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png"
-    },
-    {
-      id: 4,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png"
-    },
-    {
-      id: 5,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png"
-    },
-    {
-      id: 6,
-      name: "smokeScreen",
-      price: 100,
-      used: 0,
-      img: "./smokeScreen.png"
-    }
-  ],
-    	wordLetters: [],
-	seen: new Set()
+	letters: {
+		A: { id: 0, value: "A", points: 1 },
+		B: { id: 1, value: "B", points: 4 },
+		C: { id: 2, value: "C", points: 4 },
+		D: { id: 3, value: "D", points: 2 },
+		E: { id: 4, value: "E", points: 1 },
+		F: { id: 5, value: "F", points: 4 },
+		G: { id: 6, value: "G", points: 3 },
+		H: { id: 7, value: "H", points: 3 },
+		I: { id: 8, value: "I", points: 1 },
+		J: { id: 9, value: "J", points: 10 },
+		K: { id: 10, value: "K", points: 5 },
+		L: { id: 11, value: "L", points: 3 },
+		M: { id: 12, value: "M", points: 4 },
+		N: { id: 13, value: "N", points: 2 },
+		O: { id: 14, value: "O", points: 1 },
+		P: { id: 15, value: "P", points: 4 },
+		Q: { id: 16, value: "Q", points: 10 },
+		R: { id: 17, value: "R", points: 1 },
+		S: { id: 18, value: "S", points: 1 },
+		T: { id: 19, value: "T", points: 1 },
+		U: { id: 20, value: "U", points: 2 },
+		V: { id: 21, value: "V", points: 5 },
+		W: { id: 22, value: "W", points: 4 },
+		X: { id: 23, value: "X", points: 1 },
+		Y: { id: 24, value: "Y", points: 1 },
+		Z: { id: 25, value: "Z", points: 10 }
+	},
+	board: {},
+	powers: [
+		{
+			id: 1,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png"
+		},
+		{
+			id: 2,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png" // switch
+		},
+		{
+			id: 3,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png"
+		},
+		{
+			id: 4,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png"
+		},
+		{
+			id: 5,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png"
+		},
+		{
+			id: 6,
+			name: "smokeScreen",
+			price: 100,
+			used: 0,
+			img: "./smokeScreen.png"
+		}
+	],
+    wordLetters: [],
+	seen: new Set(),
+	wordScore: 0,
+	p1Score: 0,
+	p2Score: 0
 };
 
 
@@ -137,6 +145,42 @@ export const addLetter = letter => {
 	}
 }
 
+
+
+export const submitWord = (wordScore) => {
+	return (dispatch) => {
+		dispatch(clearWord())
+		dispatch(addP1Score(wordScore))
+		dispatch(clearWordScore())
+	}
+}
+
+export const clearWord = () => {
+	return {
+		type: CLEAR_WORD
+	}
+}
+
+export const addLetterScore = score => {
+	return {
+		type: ADD_LETTER_SCORE,
+		score
+	}
+}
+
+export const clearWordScore = () => {
+	return {
+		type: CLEAR_WORD_SCORE
+	}
+}
+
+export const addP1Score = score => {
+	return {
+		type: ADD_P1_SCORE,
+		score
+	}
+}
+
 // REDUCERS
 
 // keep track of what letters we've seen under the hood
@@ -154,9 +198,35 @@ export const wordReducer = (state = initialState.wordLetters, action, seen = ini
 					seen.add(letter)
 					return [...state, letter]
 				}
+				else return state
 			}
 			else return state
+		case CLEAR_WORD:
+			seen.clear()
+			return []
 		default: 
+			return state
+	}
+}
+
+export const wordScoreReducer = (state = initialState.wordScore, action) => {
+	switch (action.type) {
+		case ADD_LETTER_SCORE:
+			return state + action.score
+		case CLEAR_WORD_SCORE:
+			return 0
+		default:
+			return state
+	}
+}
+
+export const p1ScoreReducer = (state = initialState.p1Score, action) => {
+	switch (action.type) {
+		case ADD_P1_SCORE:
+			return state + action.score
+		case CLEAR_P1_SCORE:
+			return 0
+		default:
 			return state
 	}
 }
@@ -165,7 +235,6 @@ export const letterReducer = (state = initialState.letters, action) => {
   return state;
 };
 
-// export const lastLetter = ()
 
 export const boardReducer = (state = initialState.board, action) => {
   switch (action.type) {
