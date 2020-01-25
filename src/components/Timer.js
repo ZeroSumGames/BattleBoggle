9c3edc18654bf84b42209c9befae17bb51046173
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-25 11:52:00 -0500

    feature: countdown timer. install typescrip and countdown timer component from online

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,1 +1,16 @@
 import React from "react";
+import CountdownTimer from "react-component-countdown-timer";
+
+class Timer extends React.Component {
+  render() {
+    let settings = {
+      count: 5432,
+      border: true,
+      showTitle: true,
+      noPoints: true
+    };
+    return (
+      <CountdownTimer {...settings} />
+    );
+  }
+}
