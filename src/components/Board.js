0119cda6b02f9d5af7668e1f94c5350385a764df
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-25 15:01:13 -0500

    fix grid bug and align properly in the center

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,71 +1,86 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import LetterTile from './LetterTile'
-import {linearize} from '../utilities'
-import ReactDOM from 'react-dom'
-import {submitWord, clearWordScore, addP1Score, clearWord} from '../store/game'
+import React from "react";
+import { connect } from "react-redux";
+import LetterTile from "./LetterTile";
+import { linearize } from "../utilities";
+import ReactDOM from "react-dom";
+import {
+  submitWord,
+  clearWordScore,
+  addP1Score,
+  clearWord
+} from "../store/game";
 
 // import {makeBoard} from '../store/game'
 import "./style/Board.css";
 
 class Board extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			mouseIsDown: false
-		}
-	}
+  constructor() {
+    super();
+    this.state = {
+      mouseIsDown: false
+    };
+  }
 
+  componentDidMount() {
+    // ReactDOM.findDOMNode(this).addEventListener('')
+    // this.clicked = this.clicked.bind(this)
+  }
 
-	componentDidMount() {
-		// ReactDOM.findDOMNode(this).addEventListener('')
-		// this.clicked = this.clicked.bind(this)
-	}
+  toggleMouseDown(event) {
+    this.setState({ mouseIsDown: !this.state.mouseIsDown });
+  }
 
-	toggleMouseDown(event) {
-		this.setState({mouseIsDown: !this.state.mouseIsDown})
-	}
+  toggleMouseUp(event) {
+    this.setState({ mouseIsDown: !this.state.mouseIsDown });
+    // this.props.checkWord(this.props.wordScore)
 
-	toggleMouseUp(event) {
-		this.setState({mouseIsDown: !this.state.mouseIsDown})
-		// this.props.checkWord(this.props.wordScore)
+    this.props.addP1Score(this.props.wordScore);
+    this.props.clearWord();
+    this.props.clearWordScore();
 
-		this.props.addP1Score(this.props.wordScore)
-		this.props.clearWord()
-		this.props.clearWordScore()
+    console.log("fdsfsdf");
+  }
 
-		console.log("fdsfsdf");
-	}
+  render() {
+    let board = this.props.board;
 
-	render() {
-		let board = this.props.board;
-
-		return (
-			<div onMouseDown={(event) => this.toggleMouseDown(event)} onMouseUp={(event) => this.toggleMouseUp(event)}>
-				{board && board.map((letter,i) => {
-					return (
-						<LetterTile mouseIsDown={this.state.mouseIsDown} letter={letter} key={i} row={Math.floor(i/4)} col={i%4}/>
-					)
-				})}
-			</div>
-		)
-	}
+    return (
+      <div
+        className="board"
+        onMouseDown={event => this.toggleMouseDown(event)}
+        onMouseUp={event => this.toggleMouseUp(event)}
+      >
+        {board &&
+          board.map((letter, i) => {
+            return (
+              <LetterTile
+                mouseIsDown={this.state.mouseIsDown}
+                letter={letter}
+                key={i}
+                row={Math.floor(i / 4)}
+                col={i % 4}
+              />
+            );
+          })}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		board: state.board,
-		wordScore: state.wordScore
-	}
-}
+  return {
+    board: state.board,
+    wordScore: state.wordScore
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-	return {
-		checkWord: wordScore => submitWord(wordScore),
-		addP1Score: wordScore => dispatch(addP1Score(wordScore)),
-		clearWord: () => dispatch(clearWord()),
-		clearWordScore: () => dispatch(clearWordScore())
-	}
-}
+  return {
+    checkWord: wordScore => submitWord(wordScore),
+    addP1Score: wordScore => dispatch(addP1Score(wordScore)),
+    clearWord: () => dispatch(clearWord()),
+    clearWordScore: () => dispatch(clearWordScore())
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Board);
